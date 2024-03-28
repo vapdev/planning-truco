fd884c4f3e5c63ff5250d3256e5621fceae52a5e
Vitor <vpbohn@gmail.com>
2024-03-28 20:06:55 -0300

    style: code format

--- app.vue
+++ app.vue
@@ -5,10 +5,8 @@
         <template v-if="jogoComecou && jogadorLogado.admin">
           <div class="flex flex-col gap-4">
             <div class="flex gap-2">
-              <div
-                @click="virarAutomatico = !virarAutomatico"
-                class="hover:bg-gray-500 w-7 rounded-lg cursor-pointer h-7 border-2 border-white justify-center flex items-center"
-              >
+              <div @click="virarAutomatico = !virarAutomatico"
+                class="hover:bg-gray-500 w-7 rounded-lg cursor-pointer h-7 border-2 border-white justify-center flex items-center">
                 <div v-if="virarAutomatico" class="font-bold text-green-400 text-xl">
                   ✓
                 </div>
@@ -16,11 +14,10 @@
               <div>Virar automaticamente após todos jogadores votarem</div>
             </div>
             <div v-if="!virarAutomatico" class="text-red-500">
-              <button
-                @click="mostrarCartas = !mostrarCartas"
-                class="text-white font-bold py-2 px-4 rounded"
-                :class="mostrarCartas ? 'bg-red-500 hover:bg-red-700' : 'bg-green-500 hover:bg-green-700'"
-              >
+              <button @click="mostrarCartas = !mostrarCartas" class="text-white font-bold py-2 px-4 rounded" :class="mostrarCartas
+          ? 'bg-red-500 hover:bg-red-700'
+          : 'bg-green-500 hover:bg-green-700'
+          ">
                 <span v-if="mostrarCartas">Esconder cartas</span>
                 <span v-else>Mostrar cartas</span>
               </button>
@@ -32,23 +29,15 @@
       <div class="flex-grow flex-col flex gap-2">
         <template v-if="!jogoComecou">
           <div class="flex justify-end">
-            <button
-              @click="startGame"
-              class="bg-green-500 w-40 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-            >
+            <button @click="startGame"
+              class="bg-green-500 w-40 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
               Iniciar nova sala
             </button>
           </div>
           <div class="flex justify-end gap-2">
-            <input
-              v-model="gameId"
-              class="border-2 border-green-500 rounded pl-2"
-              placeholder="Digite o ID da sala"
-            />
-            <button
-              @click="loadGame"
-              class="bg-green-500 w-40 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-            >
+            <input v-model="gameId" class="border-2 border-green-500 rounded pl-2" placeholder="Digite o ID da sala" />
+            <button @click="loadGame"
+              class="bg-green-500 w-40 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
               Entrar em sala
             </button>
           </div>
@@ -57,40 +46,23 @@
           <div class="text-white text-xl mr-6">
             ID da sala: <span class="font-bold">{{ gameId }}</span>
           </div>
-          <button
-            @click="sairDaSala"
-            class="bg-red-500 w-40 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-          >
+          <button @click="sairDaSala" class="bg-red-500 w-40 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
             Sair da sala
           </button>
         </div>
         <div class="flex justify-end h-10">
-          <input
-            v-model="nome"
-            class="border-2 border-green-500 w-40 rounded pl-2"
-            placeholder="Seu nome aqui"
-          />
+          <input v-model="nome" class="border-2 border-green-500 w-40 rounded pl-2" placeholder="Seu nome aqui" />
         </div>
       </div>
     </div>
     <template v-if="jogoComecou">
       <div class="w-full flex justify-center gap-10">
-        <div
-          v-for="(player, index) in players"
-          :key="index"
-          class="mt-10 flex-col flex gap-2"
-        >
-          <div
-            class="w-28 h-40 flex-col flex rounded-lg justify-center border-8"
-            :class="
-              player.votou
-                ? 'border-green-500 bg-green-100'
-                : 'border-yellow-300 bg-yellow-100'
-            "
-          >
-            <span v-if="mostrarCartas && player.votou" class="text-5xl flex justify-center">{{
-              player.score
-            }}</span>
+        <div v-for="(player, index) in players" :key="index" class="mt-10 flex-col flex gap-2">
+          <div class="w-28 h-40 flex-col flex rounded-lg justify-center border-8" :class="player.votou
+          ? 'border-green-500 bg-green-100'
+          : 'border-yellow-300 bg-yellow-100'
+          ">
+            <span v-if="mostrarCartas && player.votou" class="text-5xl flex justify-center">{{ player.score }}</span>
           </div>
           <div>
             <span class="text-2xl flex justify-center text-white">{{ player.name }}</span>
@@ -98,18 +70,10 @@
         </div>
       </div>
       <div class="w-full flex justify-center gap-4">
-        <div
-          v-for="(card, index) in fibonacciCards"
-          :key="index"
-          class="mt-10 flex-col flex gap-2 text-5xl hover:text-6xl hover:scale-[1.15]"
-        >
-          <div
-            @click="votar(card.number)"
-            :class="
-              selectedCard == card.number ? card.class + ' -translate-y-10' : card.class
-            "
-            class="w-24 h-32 flex-col flex rounded-lg justify-center cursor-pointer border-gray-400 border-4"
-          >
+        <div v-for="(card, index) in fibonacciCards" :key="index"
+          class="mt-10 flex-col flex gap-2 text-5xl hover:text-6xl hover:scale-[1.15]">
+          <div @click="votar(card.number)" :class="selectedCard == card.number ? card.class + ' -translate-y-10' : card.class
+          " class="w-24 h-32 flex-col flex rounded-lg justify-center cursor-pointer border-gray-400 border-4">
             <span class="flex justify-center">{{ card.number }}</span>
           </div>
         </div>
