e5f53e37aa46a252c220fe2d715c0181347f0ad4
Vitor <vpbohn@gmail.com>
2024-04-07 03:34:19 -0300

    feat: criacao do esqueleto landing page

--- app.vue
+++ app.vue
@@ -1,9 +1,73 @@
 <template>
-  <div class="h-[100vh] w-[100vw] flex flex-col justify-between p-4 md:p-16 md:pb-4">
-    <div
-      class="oswald-font text-4xl md:mb-0 text-center w-full md:w-fit text-white absolute md:top-13 left-1/2 transform -translate-x-1/2">
-      Free Planning Poker</div>
-    <div class="w-full md:grid md:grid-cols-2 flex flex-col mt-16 md:mt-0">
+  <div class="h-[100vh] w-[100vw] flex flex-col">
+    <!-- HEADER -->
+    <div class="w-full px-4 flex justify-between text-white items-center text-2xl bg-preto h-28">
+      <div class="oswald-font text-green-500">Free Planning Poker</div>
+      <div class="text-lg font-bold">
+        <div class="flex gap-8">
+          <div>Sign Up</div>
+          <div>Login</div>
+          <button @click="startGame"
+            class="bg-green-500 w-60 md:hover:bg-green-400 font-bold px-4 rounded">
+            Iniciar nova sala
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <!-- MAIN CONTENT -->
+    <div class="flex justify-around h-full px-28 py-32">
+      <div class="text-white text-xl">
+        <p class="text-4xl mb-8">Bem-vindo ao <span class="underline-offset-4 underline text-green-500">Free Planning Poker</span>!</p>
+
+        <ul class="mb-8 list-disc pl-6">
+          <li>Simples</li>
+          <li>Em tempo real</li>
+          <li>Acesse de qualquer dispositivo</li>
+          <li>Jogue como convidado ou crie sua conta</li>
+          <li class="mb-8">Sem limite de partidas, votos ou usuários</li>
+          <li>Adicione o nome das tasks para votação (PREMIUM)</li>
+          <li>Tenha acesso ao histórico das votações (PREMIUM)</li>
+          <li>Exporte os resultados para CSV e Excel (PREMIUM)</li>
+        </ul>
+
+        <p class="my-32">Tenha acesso aos recursos <span class="text-green-500 cursor-pointer underline">Premium</span>
+        </p>
+        <div class="flex align-center gap-2">
+          <p class="text-xs">Criado por:</p>
+          <div class="cursor-pointer flex align-center gap-2">
+            <p class="text-xs">vapdev </p>
+            <Icon name="uil:github" color="black" />
+          </div>
+        </div>
+      </div>
+      <div class="text-white text-2xl">
+        <p class="text-3xl mb-4 font-bold">Jogue agora gratuitamente!</p>
+        <div class="flex font-bold text-xl py-8 gap-16">
+          <template v-if="!jogoComecou">
+            <div class="flex flex-col gap-6 text-white">
+              <p>Crie uma nova sala</p>
+              <button @click="startGame"
+                class="w-60 bg-green-500 md:hover:bg-green-400 text-white font-bold py-2 px-4 rounded">
+                Iniciar nova sala
+              </button>
+            </div>
+            <div class="flex flex-col gap-6 text-white">
+              <div>Ou entre em uma sala existente</div>
+              <div>
+                <input v-model="roomUUID" class="border-2 h-12 border-green-500 rounded pl-2"
+                  placeholder="Insira o link da sala" />
+              </div>
+            </div>
+          </template>
+          <!-- <div class="flex h-10">
+          <input v-model="nome" class="border-2 w-40 rounded pl-2" placeholder="Seu nome aqui" />
+        </div> -->
+        </div>
+      </div>
+    </div>
+
+    <div class="w-full bg-cinza md:grid md:grid-cols-2 flex flex-col mt-16 md:mt-0">
       <div class="w-full">
         <div v-if="jogoComecou" class="flex-col flex gap-4">
           <div class="text-white text-xl w-full md:w-fit mr-6">
@@ -13,28 +77,6 @@
             class="bg-red-500 w-40 mb-4 md:mb-0 md:hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
             Sair da sala
           </button>
-        </div>
-      </div>
-      <div class="flex-grow flex-col flex gap-2">
-        <template v-if="!jogoComecou">
-          <div class="flex md:justify-end">
-            <button @click="startGame"
-              class="bg-green-500 w-40 md:hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-              Iniciar nova sala
-            </button>
-          </div>
-          <div class="flex md:justify-end gap-2">
-            <input v-model="roomUUID" class="border-2 border-green-500 rounded pl-2"
-              placeholder="Digite o nome da sala" />
-            <button @click="loadGame"
-              class="bg-green-500 w-40 md:hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-              Entrar em sala
-            </button>
-          </div>
-        </template>
-
-        <div class="flex md:justify-end h-10">
-          <input v-model="nome" class="border-2 w-40 rounded pl-2" placeholder="Seu nome aqui" />
         </div>
       </div>
     </div>
@@ -63,7 +105,8 @@
               </button>
             </div>
             <div v-if="roomState.showCards">
-              <button @click="novaRodada" class="w-50 md:hover:bg-blue-600 text-white font-bold py-2 px-4 rounded bg-blue-500">
+              <button @click="novaRodada"
+                class="w-50 md:hover:bg-blue-500 text-white font-bold py-2 px-4 rounded bg-blue-500">
                 <span>Iniciar nova votação</span>
               </button>
             </div>
@@ -98,6 +141,7 @@ html {
   height: 100vh;
   background-color: #3f4146;
 }
+
 .oswald-font {
   font-family: "Oswald", sans-serif;
   font-optical-sizing: auto;
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     '@nuxtjs/i18n',
     '@nuxtjs/google-fonts',
+    'nuxt-icon',
   ],
   runtimeConfig: {
     public: {
