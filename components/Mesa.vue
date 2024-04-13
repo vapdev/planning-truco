e49b3852dab2b4037bb34ecc8d86b66f348cd253
Vitor <vpbohn@gmail.com>
2024-04-13 02:23:45 -0300

    Update UI styling and fix bugs in BottomContainer.vue and Mesa.vue components

--- components/Mesa.vue
+++ components/Mesa.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="justify-center flex flex-wrap w-full gap-10 md:mx-8">
-        <div class="md:w-64 w-40 h-24 rounded-xl flex justify-center items-center bg-gray-400 mx-4 md:m-10">
+    <div class="justify-center flex flex-wrap w-full mx-4">
+        <div class="md:w-64 w-44 h-24 rounded-xl flex justify-center items-center bg-gray-400">
             <div v-if="!userStore.roomState.autoShowCards && !userStore.roomState.showCards" class="text-red-500">
                 <button @click="emit('toggleMostrarCartas')"
                     class="text-white font-bold py-2 px-4 rounded w-50 bg-blue-500 hover:bg-blue-400">
@@ -9,8 +9,8 @@
             </div>
             <div v-if="userStore.roomState.showCards">
                 <button @click="emit('novaRodada')"
-                    class="w-50 md:hover:bg-blue-500 text-white font-bold py-2 px-4 rounded bg-blue-500">
-                    <span>Iniciar nova votação</span>
+                    class="w-32 md:w-50 md:hover:bg-blue-500 text-white font-bold py-2 px-4 rounded bg-blue-500">
+                    Iniciar nova votação
                 </button>
             </div>
         </div>
