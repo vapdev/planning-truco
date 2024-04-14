cd251656e100501a1b24ab7a12cee29983042149
Vitor <vpbohn@gmail.com>
2024-04-14 12:35:02 -0300

    Update UI styling and fix bugs in HeaderSala.vue component

--- components/HeaderSala.vue
+++ components/HeaderSala.vue
@@ -1,27 +1,32 @@
 <template>
-    <div class="text-white text-lg flex p-4 items-start justify-between">
-        <div class="w-1/3 justify-start items-center">
-            <div class="flex gap-2 items-center">
-                <div v-if="$md" class="text-sm">Copiar link da sala:</div>
-                <div @click="copyToClipboard" class="hover:bg-gray-500 hover:cursor-pointer rounded-xl p-1">
-                    <Icon size="30" name="material-symbols:content-copy" color="white" />
+    <div>
+        <div class="text-white text-lg flex p-4 items-start justify-between">
+            <div class="w-1/3 justify-start items-center">
+                <div class="flex gap-2 items-center">
+                    <div v-if="$md" class="text-sm">Copiar link da sala:</div>
+                    <div @click="copyToClipboard" class="hover:bg-gray-500 hover:cursor-pointer rounded-xl p-1">
+                        <Icon size="30" name="material-symbols:content-copy" color="white" />
+                    </div>
+                </div>
+            </div>
+            <div class="flex w-1/3 justify-center items-center">
+                <span class="invisible md:visible font-bold">{{ userStore.roomState.name }}</span>
+            </div>
+            <div class="flex w-1/3 justify-end gap-5 items-center">
+                <div class="hover:bg-gray-500 hover:cursor-pointer rounded-xl p-1">
+                    <Icon @click="abrirModalCompartilhar" size="30" name="material-symbols:share" color="white" />
+                </div>
+                <div @click="modalConfig = true" class="p-1 hover:bg-gray-500 rounded-xl">
+                    <Icon class="hover:cursor-pointer" size="30" name="material-symbols:settings" color="white" />
+                </div>
+                <div class="hover:bg-gray-500 hover:cursor-pointer rounded-xl p-1">
+                    <Icon @click="emit('endGame')" size="30" name="material-symbols:exit-to-app" color="white" />
                 </div>
             </div>
         </div>
-        <div class="flex w-1/3 justify-center items-center">
-            <span class="font-bold">{{ userStore.roomState.name }}</span>
-        </div>
-        <div class="flex w-1/3 justify-end gap-5 items-center">
-            <div class="hover:bg-gray-500 hover:cursor-pointer rounded-xl p-1">
-                <Icon @click="abrirModalCompartilhar" size="30" name="material-symbols:share" color="white" />
-            </div>
-            <div @click="modalConfig = true" class="p-1 hover:bg-gray-500 rounded-xl">
-                <Icon class="hover:cursor-pointer" size="30" name="material-symbols:settings" color="white" />
-            </div>
-            <div class="hover:bg-gray-500 hover:cursor-pointer rounded-xl p-1">
-                <Icon @click="emit('endGame')" size="30" name="material-symbols:exit-to-app" color="white" />
-            </div>
-        </div>
+
+        <div><span class="md:invisible font-bold w-full text-white flex justify-center">{{ userStore.roomState.name }}</span></div>
+
         <UModal :ui="{ overlay: { background: 'bg-blue-200/40' } }" v-model="modalShare">
             <div class="relative rounded-lg font-bold bg-[#3f4146] p-12 flex flex-col gap-6">
                 <div class="absolute cursor-pointer text-white flex top-2 right-2">
@@ -56,7 +61,7 @@
                 <div class="text-md flex flex-col gap-4 text-white">
                     <div>Seu nome:</div>
                     <div class="flex w-full gap-4">
-                        <UInput v-model="userName" size="lg" color="blue"  class="w-full" placeholder="Seu nome" />
+                        <UInput v-model="userName" size="lg" color="blue" class="w-full" placeholder="Seu nome" />
                     </div>
                 </div>
                 <button @click="userStore.changeName(userName); modalConfig = false"
