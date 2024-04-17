95a4a0c5d7a91c6909d23044524b86c55a0529d2
Vitor <vpbohn@gmail.com>
2024-04-17 19:34:48 -0300

    Refactor UI styling and fix bugs in Stats.vue component

--- components/Stats.vue
+++ components/Stats.vue
@@ -14,12 +14,16 @@
                 <div class="font-semibold mt-1">Média: {{ stats.average }}</div>
                 <div class="font-semibold mt-1">Assertividade: {{ stats.assertiveness }}%</div>
             </div>
-            <div class="grid grid-cols-2 gap-x-4 gap-y-2">
-                <div v-for="card in nonRepeatedCardsWithVotes" class="flex items-center gap-2" :key="card.value">
-                    <div class="w-5 h-7 text-black oswald-font font-bold flex rounded-md justify-center bg-green-100">
-                        <span class="text-lg flex justify-center"><span>{{ card.value == -1 ? '☕' : card.value }}</span></span>
+            <div>
+                <div class="flex flex-col gap-2">
+                    <div v-for="card in nonRepeatedCardsWithVotes" class="flex gap-2 items-center" :key="card.value">
+                        <div
+                            class="w-5 h-7 text-black oswald-font font-bold rounded-md justify-center bg-green-100">
+                            <span class="text-lg flex justify-center"><span>{{ card.value == -1 ? '☕' : card.value
+                                    }}</span></span>
+                        </div>
+                        <div class="text-md">{{ card.vote }} {{ card.vote === 1 ? 'voto' : 'votos' }}</div>
                     </div>
-                    <div class="text-md">{{ card.vote }} {{ card.vote === 1 ? 'voto' : 'votos' }}</div>
                 </div>
             </div>
         </div>
@@ -70,9 +74,9 @@ const stats = computed(() => {
 
         const mostVoted = mostVotedSorted.filter(([card, count]) => count === mostVotedSorted[0][1]).map(([card]) => card);
 
-        const votesWithoutMinusOne = votes.filter(vote => vote !== -1);
-        const totalVotes = votesWithoutMinusOne.length;
-        const sumOfVotes = votesWithoutMinusOne.reduce((a, b) => a + b, 0);
+        const votesWithoutNull = votes.filter(vote => vote !== null);
+        const totalVotes = votesWithoutNull.length;
+        const sumOfVotes = votesWithoutNull.reduce((a, b) => a + b, 0);
         const average = totalVotes ? sumOfVotes / totalVotes : 0;
 
         const assertiveness = (mostVotedSorted[0][1] / votes.length) * 100;
