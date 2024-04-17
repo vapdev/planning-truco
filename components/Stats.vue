5f37c30e65e4f48724d60d827d3eaa507dbfb729
Vitor <vpbohn@gmail.com>
2024-04-17 19:11:56 -0300

    Refactor UI styling and fix bugs in Stats.vue component

--- components/Stats.vue
+++ components/Stats.vue
@@ -8,22 +8,16 @@
             <div class="text-md items-start">
                 <div class="flex gap-2 items-end font-semibold">
                     <div>
-                        Carta mais votada: {{ stats.mostVoted.map(card => card).join(', ') }}
+                        Carta mais votada: {{ stats.mostVoted.map(card => card == -1 ? '☕' : card).join(', ') }}
                     </div>
-                    
                 </div>
                 <div class="font-semibold mt-1">Média: {{ stats.average }}</div>
                 <div class="font-semibold mt-1">Assertividade: {{ stats.assertiveness }}%</div>
             </div>
-
-            <div style="height:88px; width: 1px; background-color: white;">
-
-            </div>
-
             <div class="grid grid-cols-2 gap-x-4 gap-y-2">
                 <div v-for="card in nonRepeatedCardsWithVotes" class="flex items-center gap-2" :key="card.value">
                     <div class="w-5 h-7 text-black oswald-font font-bold flex rounded-md justify-center bg-green-100">
-                        <span class="text-lg flex justify-center"><span>{{ card.value }}</span></span>
+                        <span class="text-lg flex justify-center"><span>{{ card.value == -1 ? '☕' : card.value }}</span></span>
                     </div>
                     <div class="text-md">{{ card.vote }} {{ card.vote === 1 ? 'voto' : 'votos' }}</div>
                 </div>
@@ -50,7 +44,7 @@ const userStore = useUserStore();
 const notNullVotes = computed(() => userStore.players.filter(player => player.vote !== -1 || player.vote !== 0));
 
 const nonRepeatedCardsWithVotes = computed(() => {
-    const notNullVotes = userStore.players.filter(player => player.vote !== -1 && player.vote !== 0);
+    const notNullVotes = userStore.players.filter(player => player.vote !== null);
     const votes = notNullVotes.map(player => player.vote);
     const voteCounts = votes.reduce((acc, curr) => {
         acc[curr] = (acc[curr] || 0) + 1;
@@ -76,11 +70,12 @@ const stats = computed(() => {
 
         const mostVoted = mostVotedSorted.filter(([card, count]) => count === mostVotedSorted[0][1]).map(([card]) => card);
 
-        const totalVotes = votes.length;
-        const sumOfVotes = votes.reduce((a, b) => a + b, 0);
-        const average = sumOfVotes / totalVotes;
+        const votesWithoutMinusOne = votes.filter(vote => vote !== -1);
+        const totalVotes = votesWithoutMinusOne.length;
+        const sumOfVotes = votesWithoutMinusOne.reduce((a, b) => a + b, 0);
+        const average = totalVotes ? sumOfVotes / totalVotes : 0;
 
-        const assertiveness = (mostVotedSorted[0][1] / totalVotes) * 100;
+        const assertiveness = (mostVotedSorted[0][1] / votes.length) * 100;
 
         return {
             mostVoted,
