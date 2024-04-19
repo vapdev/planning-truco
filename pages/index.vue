0440695fe4a5cfffbd0c260fbb2765e71f0aaf9d
Vitor <vpbohn@gmail.com>
2024-04-18 23:18:40 -0300

    Update UI styling and fix bugs in index.vue component

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
 
     <div class="overflow-x-hidden antialiased">
         <!-- Header Section -->
-        <header class="relative z-[53] w-full h-24">
+        <header class="relative w-full h-24">
             <div
                 class="container flex items-center justify-center h-full max-w-6xl px-8 mx-auto sm:justify-between xl:px-0">
 
@@ -38,8 +38,8 @@
                     </div>
                 </nav>
 
-                <div
-                    class="absolute left-0 flex-col items-center justify-center hidden w-full pb-8 mt-48 border-b border-gray-200 md:relative md:w-auto md:bg-transparent md:border-none md:mt-0 md:flex-row md:p-0 md:items-end md:flex md:justify-between">
+                <div 
+                    class="absolute z-[42] left-0 flex-col items-center justify-center hidden w-full pb-8 mt-48 border-b border-gray-200 md:relative md:w-auto md:bg-transparent md:border-none md:mt-0 md:flex-row md:p-0 md:items-end md:flex md:justify-between">
                     <!-- <a href="#_"
                         class="relative z-40 px-3 py-2 mr-0 text-sm font-bold text-pink-500 md:px-5 lg:text-white sm:mr-3 md:mt-0">Login</a> -->
                     <a @click="startGame"
