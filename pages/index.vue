eccafc2b00c0158f57429e1d308c8fe9d8efa213
Vitor <vpbohn@gmail.com>
2024-04-18 23:04:37 -0300

    Fix UI styling and enable start game button in index.vue component

--- pages/index.vue
+++ pages/index.vue
@@ -29,8 +29,9 @@
                     <a href="#testimonials"
                         class="font-bold duration-100 transition-color hover:text-indigo-600">Depoimentos</a> -->
                     <div class="flex flex-col block w-full font-medium border-t border-gray-200 md:hidden">
-                        <a href="#_" class="w-full py-2 font-bold text-center text-pink-500">Login</a>
-                        <a href="#_"
+                        <!-- <a href="#_" class="w-full py-2 font-bold text-center text-pink-500">Login</a> -->
+                        <a
+                            @click="startGame"
                             class="relative inline-block w-full px-5 py-3 text-sm leading-none text-center text-white bg-indigo-700 fold-bold">
                             Iniciar agora
                         </a>
@@ -41,7 +42,7 @@
                     class="absolute left-0 flex-col items-center justify-center hidden w-full pb-8 mt-48 border-b border-gray-200 md:relative md:w-auto md:bg-transparent md:border-none md:mt-0 md:flex-row md:p-0 md:items-end md:flex md:justify-between">
                     <!-- <a href="#_"
                         class="relative z-40 px-3 py-2 mr-0 text-sm font-bold text-pink-500 md:px-5 lg:text-white sm:mr-3 md:mt-0">Login</a> -->
-                    <a href="#_"
+                    <a @click="startGame"
                         class="relative z-40 inline-block w-auto h-full px-5 py-3 text-sm font-bold leading-none text-white transition-all transition duration-100 duration-300 bg-indigo-700 rounded shadow-md fold-bold lg:bg-white lg:text-indigo-700 sm:w-full lg:shadow-none hover:shadow-xl">Iniciar agora</a>
                     <svg class="absolute top-0 left-0 hidden w-screen max-w-3xl -mt-64 -ml-12 lg:block"
                         viewBox="0 0 818 815" xmlns="http://www.w3.org/2000/svg"
