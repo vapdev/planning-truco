3414ff5f10569f5a8618a11d5828d54a4d2c6ef7
Vitor <vpbohn@gmail.com>
2025-09-22 18:31:32 -0300

    feat: Add devServer configuration for local development

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000
+  },
 
   modules: [
     '@nuxt/ui',
